f032cc1ea0ff78333a258b3f07ccb153cf0b4d3a
alibayomy <alibayomy12@gmail.com>
2023-10-20 10:50:04 -0700

    adding the bubble sort alogorithm answer

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,46 +1,25 @@
 #include "sort.h"
 /**
- * sorted -> check if the given array is already sorted
- * @array: the given array
- * @size: the size of the array
- * Return: 0 if the given is sorted, else 1
-*/
-int sorted(int *array, size_t size)
-{
-	int i = 0;
-
-	while (i < (int) size)
-	{
-		if (array[i] > array[i + 1])
-		{
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-/**
  * bubble_sort -> sorting an array of ints using bubble sort alogrthim
  * @array: the given array containing the ints
  * @size: the size of  the array
 */
 void bubble_sort(int *array, size_t size)
 {
-	int i = 0, tmp;
+	int i, tmp, y;
 
-	while (i < (int)size)
+	for (i = 0; i < (int) size; i++)
 	{
-		if (array[i] > array[i + 1])
+
+		for (y = 0; y < (int) size - 1; y++)
 		{
-			tmp = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = tmp;
-			print_array(array, size);
+			if (array[y] > array[y  + 1])
+			{
+				tmp = array[y];
+				array[y] = array[y + 1];
+				array[y + 1] = tmp;
+				print_array(array, size);
+			}
 		}
-		i++;
-	}
-	if (sorted(array, size) == 1)
-	{
-		bubble_sort(array, size);
 	}
 }
